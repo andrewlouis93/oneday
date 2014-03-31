81e9cf4c5d944e52216a1f102b5d38fa28be5b36
Andrew Louis <andrew.louis93@gmail.com>
2014-03-31 08:09:04 -0400

    Beta Commit

--- oneday/app/controllers/todos_controller.rb
+++ oneday/app/controllers/todos_controller.rb
@@ -60,4 +60,14 @@ class TodosController < ApplicationController
 	def about
 	end
 
+	def stats
+		@today_data = Todo.where(done:true,user:current_user.email).where("updated_at >= ?", Time.zone.now.beginning_of_day)
+		@today = Todo.where(done:true,user:current_user.email).where("updated_at >= ?", Time.zone.now.beginning_of_day).size
+		
+		@week_data = Todo.where(done:true,user:current_user.email).where("updated_at >= ?", 7.days.ago)
+		@week = Todo.where(done:true,user:current_user.email).where("updated_at >= ?", Time.zone.now.beginning_of_week).size
+		
+		@year_data = Todo.where(done:true,user:current_user.email).where("updated_at >= ?", Time.zone.now.beginning_of_year)
+		@year = Todo.where(done:true,user:current_user.email).where("updated_at >= ?", Time.zone.now.beginning_of_year).size
+	end
 end
--- oneday/config/routes.rb
+++ oneday/config/routes.rb
@@ -15,6 +15,7 @@ Oneday::Application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   get 'about' => 'todos#about'
+  get 'stats' => 'todos#stats'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
